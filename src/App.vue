0e6f60b398faa68eb9f92088de816a7bbb6defb8
Miss Taco <misstaco@msn.com>
2025-04-21 18:29:34 -0400

    add: font-awesome

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TheNavigation from './components/TheNavigation.vue'
+import TheFooter from './components/TheFooter.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  <TheNavigation />
+  
   <main>
-    <TheWelcome />
+    <router-view />
   </main>
+  
+  <TheFooter />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+@import './assets/base.css';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+main {
+  min-height: 100vh;
+  width: 100%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+html {
+  scroll-behavior: smooth;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,64 @@
 import './assets/main.css'
+import './assets/styles/index.css'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { 
+  faSun, 
+  faCarrot, 
+  faSpa, 
+  faBed, 
+  faWalking, 
+  faNotesMedical, 
+  faSyncAlt, 
+  faAllergies, 
+  faBone, 
+  faBars, 
+  faTimes,
+  faInfoCircle,
+  faUsers,
+  faClipboardList,
+  faUserMd,
+  faChartLine,
+  faHeartbeat
+} from '@fortawesome/free-solid-svg-icons'
+import { faCalendarCheck } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(
+  faSun, 
+  faCarrot, 
+  faSpa, 
+  faBed, 
+  faWalking, 
+  faNotesMedical, 
+  faSyncAlt, 
+  faAllergies, 
+  faBone,
+  faCalendarCheck,
+  faBars,
+  faTimes,
+  faInfoCircle,
+  faUsers,
+  faClipboardList,
+  faUserMd,
+  faChartLine,
+  faHeartbeat
+)
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+/* add font awesome icon component */
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(router)
+app.mount('#app')
